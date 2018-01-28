b97274862532e5dcff6896777a4af7919c059aaa
Joy Hauser <joy.hauser@hotmail.com>
2018-01-28 12:22:55 -0600

    Update the cards to navigate to the link when clicked

--- client/src/CurriculumElement.js
+++ client/src/CurriculumElement.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 import './Page.css';
+import './Card.css';
 
 class CurriculumElement extends Component {
+	constructor(props) {
+		super(props);
+	}
+	
 	componentDidMount() {
 		console.log('elem')
 	}
 	
+	goToLink(link) {
+		console.log(link);
+		window.location=link;
+		/*window.location="http://www.google.com";*/
+	}
+	
 	render() {
+		var divStyle = {
+            backgroundColor: this.props.color
+        }
+		
+		var link = this.props.link;
+		console.log(link);
+		
 		return (
-			<div className="CurriculumElement">
-				{this.props.id} <br />
-				{this.props.name} <br />
-				{this.props.description} <br />
-				{this.props.link} <br />
-				{this.props.color}
+			<div className="CurriculumElement card" onClick={() => this.goToLink(link)}>
+				<div className="card-img" style={divStyle}></div>
+				<div className="card-text">
+					{this.props.name}
+				</div>
 			</div>
 		);
 	}
