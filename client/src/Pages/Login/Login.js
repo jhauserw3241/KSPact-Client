e1213d22cdf12a73af89831d29b52eb934cac28a
Joy Hauser <joy.hauser@hotmail.com>
2018-02-16 12:51:34 -0600

    Update login page to redirect on successful login

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import  { Redirect } from 'react-router-dom';
 import fire from './../../fire';
 
 class Login extends Component {
     constructor(props) {
 		super(props);
 		this.state = {
+            redirect: false,
 			email: "",
 			password: ""
 		};
@@ -12,7 +14,7 @@ class Login extends Component {
 		this.handleEmailChange = this.handleEmailChange.bind(this);
 		this.handlePasswordChange = this.handlePasswordChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
-	}
+    }
 
     handleEmailChange(event) {
         this.setState({email: event.target.value});
@@ -25,8 +27,14 @@ class Login extends Component {
     handleSubmit(event) {
         event.preventDefault();
 
-        fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).catch(function(error) {
-            // Handle Errors here.
+        var self = this;
+
+        fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+        .then(function(firebaseUser) {
+            // Handle user having logged in succesfully
+            self.setState({ redirect: true });
+        })
+        .catch(function(error) {
             var errorCode = error.code;
             var errorMessage = error.message;
             // ...
@@ -52,23 +60,29 @@ class Login extends Component {
     }
 
 	render() {
-        return (
-			<div className="Home">
-				<div className="container">
-					<form method="POST" onSubmit={this.handleSubmit}>
-                        <fieldset>
-                            <label htmlFor="email">Email:</label>
-                            <input type="text" id="email" onChange={this.handleEmailChange} />
-                        </fieldset>
-                        <fieldset>
-                            <label htmlFor="password">Password:</label>
-                            <input type="password" id="password" onChange={this.handlePasswordChange} />
-                        </fieldset>
-                        <input type="submit" value="Submit" />
-                    </form>
-				</div>
-			</div>
-		);
+        if(this.state.redirect) {
+            return (
+                <Redirect to ="/home" />
+            );
+        } else  {
+            return (
+                <div className="Home">
+                    <div className="container">
+                        <form method="POST" onSubmit={this.handleSubmit}>
+                            <fieldset>
+                                <label htmlFor="email">Email:</label>
+                                <input type="text" id="email" onChange={this.handleEmailChange} />
+                            </fieldset>
+                            <fieldset>
+                                <label htmlFor="password">Password:</label>
+                                <input type="password" id="password" onChange={this.handlePasswordChange} />
+                            </fieldset>
+                            <input type="submit" value="Submit" />
+                        </form>
+                    </div>
+                </div>
+            );
+        }
 	}
 }
 
