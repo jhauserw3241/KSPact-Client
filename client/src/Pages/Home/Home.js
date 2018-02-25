d7ce71b1dc3a21b78c91eb63c7c30c8071279cf4
Joy Hauser <joy.hauser@hotmail.com>
2018-02-25 14:42:28 -0600

    Move checking credentials for additional feature to the LoginRequired class

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,52 +1,22 @@
 import React, { Component } from 'react';
-import fire from './../../fire';
+import LoginRequired from './../Login/LoginRequired';
 
 class Home extends Component {
 	render() {
-		/*fire.auth().onAuthStateChanged(
-            function(user) {
-                console.log("Authentication changed");
-                if (user) {
-                    // User is signed in.
-                    alert("Email: " +  user.email);
-                    user.getIdToken().then(function(accessToken) {
-						alert("User is signed in");
-                    });
-                } else {
-					alert("User is signed out");
-				}
-            },
-            function(error) {
-                console.log(error);
-            }
-        );*/
-
-		var user = fire.auth().currentUser;
-		if(user) {
-			// User is signed in
-			return (
-				<div className="Home">
-					<div className="container">
-						<div className="content">
-							This is the home page. Welcome!<br/>
+		return (
+			<div className="Home">
+				<div className="container">
+					<div className="content">
+						This is the home page. Welcome!
+						<LoginRequired>
+							<br/>
 							<br/>
 							Secrets!!!!!!!!!!!!!!!!!
-						</div>
+						</LoginRequired>
 					</div>
 				</div>
-			);
-		} else {
-			// User is signed out
-			return (
-				<div className="Home">
-					<div className="container">
-						<div className="content">
-							This is the home page. Welcome!
-						</div>
-					</div>
-				</div>
-			);
-		}
+			</div>
+		);
 	}
 }
 
---
+++ client/src/Pages/Login/LoginRequired.js
@@ -0,0 +1,58 @@
+import { Component } from 'react';
+import fire from './../../fire';
+
+class LoginRequired extends Component {
+    constructor(props) {
+		super(props);
+		this.state = {
+            user: fire.auth().currentUser,
+            requiredRole: "member"
+		};
+    }
+
+    componentDidMount() {
+        // Handle changes in authentication state
+        var self = this;
+        fire.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                self.setState({user: user});    
+            } else {
+                self.setState({user: undefined});
+            }
+        });
+
+        // Check if a higher permission requirement is required to see the child information
+        if( (this.props !== undefined) &&
+            (this.props.requiredRole !== undefined) &&
+            (this.props.requiredRole !== null) &&
+            (this.props.requiredRole !== "")) {
+            this.setState({requiredRole: this.props.requiredRole});
+        }
+    }
+
+	render() {
+        // Check if a user is logged in
+        if(this.state.user) {
+            // Get the user's permission level
+            var privs = "none";
+            var membersRef = fire.database().ref("members/");
+            membersRef.orderByChild('email').equalTo(this.state.user.email).on("value", function(data) {
+                if( (data.val() !== undefined) &&
+                    (Object.keys(data.val()).length == 1)) {
+                        privs = data.val()[0].priv;
+                }
+            });
+
+            // Show the children if the user has the required permission level
+            if(this.state.requiredRole === privs) {
+                return this.props.children;
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+	}
+}
+
+export default LoginRequired;
