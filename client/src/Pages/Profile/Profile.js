3537ddc0ee4b815f55f5486f02c93ae33d468aca
Joy Hauser <joy.hauser@hotmail.com>
2018-02-25 17:55:45 -0600

    Update profile page to allow it to be editted

--- client/src/Pages/Profile/Profile.js
+++ client/src/Pages/Profile/Profile.js
@@ -8,24 +8,80 @@ class Profile extends Component {
         super(props);
 
         this.state = {
-            member: {}
+            user: fire.auth().currentUser,
+            first_name: "",
+            last_name: "",
+            email: "",
+            school: "",
+            bio: "",
+            grade_level: "",
+            title: "",
+            disable_edits: true
         }
+
+        this.toggleEditting = this.toggleEditting.bind(this);
+        this.saveChanges = this.saveChanges.bind(this);
     }
 
     componentDidMount() {
-        var user = fire.auth().currentUser;
-		if(user) {
+        var self = this;
+
+        // Handle changes in authentication state
+        fire.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                self.setState({user: user});    
+            } else {
+                self.setState({user: undefined});
+            }
+        });
+
+        // Get information about user and update state variable
+		if(this.state.user) {
             var membersRef = fire.database().ref("members/");
-            membersRef.child(user.uid).on("value", (data) =>
-                this.setState({
-                    member: data.val()
-                    }));
+            membersRef.child(this.state.user.uid).on("value", function(data) {
+                console.log(data.val());
+                if(data.val() !== undefined) {
+                    var member = data.val();
+                    self.setState({
+                        first_name: member.first_name,
+                        last_name: member.last_name,
+                        email: member.email,
+                        school: member.school,
+                        bio: member.bio,
+                        grade_level: member.grade_level,
+                        title: member.title
+                    });
+                }
+            });
         }
     }
 
+    toggleEditting(event) {
+        this.setState({disable_edits: !this.state.disable_edits});
+    }
+
+    saveChanges(event) {
+        var newMember = {
+            first_name: this.state.first_name,
+            last_name: this.state.last_name,
+            email: this.state.email,
+            school: this.state.school,
+            bio: this.state.bio,
+            grade_level: this.state.grade_level,
+            title: this.state.title
+        };
+
+        // Update profile information
+        var updates = {};
+        updates['/members/' + this.state.user.uid] = newMember;
+        fire.database().ref().update(updates);
+
+        // Change the profile contact info to be disabled
+        this.toggleEditting(event);
+    }
+
 	render() {
-		var user = fire.auth().currentUser;
-		if(user) {
+		if(this.state.user) {
             return (
 				<div className="Home">
 					<div className="container">
@@ -34,31 +90,72 @@ class Profile extends Component {
                             <form method="POST">
                                 <fieldset>
                                     <label htmlFor="firstName">First Name:</label>
-                                    <input type="text" id="firstName" value={this.state.member.first_name} disabled />
+                                    <input
+                                        type="text"
+                                        name="firstName"
+                                        value={this.state.first_name}
+                                        onChange={(event) => this.setState({first_name: event.target.value})}
+                                        disabled={this.state.disable_edits} />
                                 </fieldset>
                                 <fieldset>
                                     <label htmlFor="lastName">Last Name:</label>
-                                    <input type="text" id="lastName" value={this.state.member.last_name} disabled />
+                                    <input
+                                        type="text"
+                                        name="lastName"
+                                        value={this.state.last_name}
+                                        onChange={(event) => this.setState({last_name: event.target.value})}
+                                        disabled={this.state.disable_edits} />
                                 </fieldset>
                                 <fieldset>
                                     <label htmlFor="email">Email:</label>
-                                    <input type="text" id="email" value={this.state.member.email} disabled />
+                                    <input
+                                        type="text"
+                                        name="email"
+                                        value={this.state.email}
+                                        onChange={(event) => this.setState({email: event.target.value})}
+                                        disabled={this.state.disable_edits} />
                                 </fieldset>
                                 <fieldset>
                                     <label htmlFor="school">School:</label>
-                                    <input type="text" id="school" value={this.state.member.school} disabled />
+                                    <input
+                                        type="text"
+                                        name="school"
+                                        value={this.state.school}
+                                        onChange={(event) => this.setState({school: event.target.value})}
+                                        disabled={this.state.disable_edits} />
                                 </fieldset>
                                 <fieldset>
                                     <label htmlFor="bio">Bio:</label>
-                                    <textarea type="text" id="bio" value={this.state.member.bio} disabled></textarea>
+                                    <textarea
+                                        type="text"
+                                        name="bio"
+                                        value={this.state.bio}
+                                        onChange={(event) => this.setState({bio: event.target.value})}
+                                        disabled={this.state.disable_edits}></textarea>
                                 </fieldset>
                                 <fieldset>
                                     <label htmlFor="gradeLevel">Grade Level:</label>
-                                    <input type="text" id="gradeLevel" value={this.state.member.grade_level} disabled />
+                                    <input
+                                        type="text"
+                                        name="gradeLevel"
+                                        value={this.state.grade_level}
+                                        onChange={(event) => this.setState({grade_level: event.target.value})}
+                                        disabled={this.state.disable_edits} />
                                 </fieldset>
                                 <fieldset>
                                     <label htmlFor="title">Title:</label>
-                                    <input type="text" id="title" value={this.state.member.title} disabled />
+                                    <input
+                                        type="text"
+                                        name="title"
+                                        value={this.state.title}
+                                        onChange={(event) => this.setState({title: event.target.value})}
+                                        disabled={this.state.disable_edits} />
+                                </fieldset>
+                                <fieldset>
+                                    { this.state.disable_edits ?
+                                        <input type="button" value="Edit" onClick={this.toggleEditting} /> : null }
+                                    { !this.state.disable_edits ?
+                                        <input type="button" value="Save" onClick={this.saveChanges} /> : null }
                                 </fieldset>
                             </form>
                         </div>
