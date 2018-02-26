2d7f398cf5f5e39c6b7e675fd873669802969cb7
Joy Hauser <joy.hauser@hotmail.com>
2018-02-25 18:37:13 -0600

    Refactor Header class to reduce code duplication

--- client/src/Pages/Header.js
+++ client/src/Pages/Header.js
@@ -5,62 +5,56 @@ import fire from './../fire.js';
 import './../CSS/Header.css';
 
 class Header extends Component {
-	render() {
-		var self = this;
+	constructor(props) {
+		super(props);
 
-		var user = fire.auth().currentUser;
-		if (user) {
-			// User is signed in
-			return (
-				<div className="Header">
-					<header>
-						<div className="Navbar">
-							<div className="Header-logo">
-								<img src={logo} className="Logo-img" alt="logo" />
-							</div>
-							<div className="nav-body">
-								<NavLink to="/home" className="nav-link">Home</NavLink>
-								<NavLink to="/members" className="nav-link">Members</NavLink>
-								<NavLink to="/hardware" className="nav-link">Hardware</NavLink>
-								<NavLink to="/software" className="nav-link">Software</NavLink>
-								<NavLink to="/curriculums" className="nav-link">Curriculums</NavLink>
-								<NavLink to="/about" className="nav-link">About</NavLink>
-								<NavLink to="/dashboard" className="nav-link">Dashboard</NavLink>
-								<NavLink to="/signout" className="nav-link">Signout</NavLink>
-							</div>
+		this.state = {
+			user: fire.auth().currentUser
+		};
+	}
+
+	componentWillMount() {
+		// Handle changes in authentication state
+        var self = this;
+        fire.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                self.setState({user: user});
+            } else {
+                self.setState({user: undefined});
+            }
+        });
+	}
+
+	render() {
+		console.log(this.state.user);
+		return (
+			<div className="Header">
+				<header>
+					<div className="Navbar">
+						<div className="Header-logo">
+							<img src={logo} className="Logo-img" alt="logo" />
 						</div>
-					</header>
-					<main>
-						{self.props.children}
-					</main>
-				</div>
-			);
-		} else {
-			// User is signed out
-			return (
-				<div className="Header">
-					<header>
-						<div className="Navbar">
-							<div className="Header-logo">
-								<img src={logo} className="Logo-img" alt="logo" />
-							</div>
-							<div className="nav-body">
-								<NavLink to="/home" className="nav-link">Home</NavLink>
-								<NavLink to="/members" className="nav-link">Members</NavLink>
-								<NavLink to="/hardware" className="nav-link">Hardware</NavLink>
-								<NavLink to="/software" className="nav-link">Software</NavLink>
-								<NavLink to="/curriculums" className="nav-link">Curriculums</NavLink>
-								<NavLink to="/about" className="nav-link">About</NavLink>
-								<NavLink to="/login" className="nav-link">Login</NavLink>
-							</div>
+						<div className="nav-body">
+							<NavLink to="/home" className="nav-link">Home</NavLink>
+							<NavLink to="/members" className="nav-link">Members</NavLink>
+							<NavLink to="/hardware" className="nav-link">Hardware</NavLink>
+							<NavLink to="/software" className="nav-link">Software</NavLink>
+							<NavLink to="/curriculums" className="nav-link">Curriculums</NavLink>
+							<NavLink to="/about" className="nav-link">About</NavLink>
+							{ this.state.user ?
+								<NavLink to="/dashboard" className="nav-link">Dashboard</NavLink> : null }
+							{ this.state.user ?
+								<NavLink to="/signout" className="nav-link">Signout</NavLink> : null }
+							{ !this.state.user ?
+								<NavLink to="/login" className="nav-link">Login</NavLink> : null }
 						</div>
-					</header>
-					<main>
-						{self.props.children}
-					</main>
-				</div>
-			);
-		}
+					</div>
+				</header>
+				<main>
+					{this.props.children}
+				</main>
+			</div>
+		);
 	}
 }
 
