6bf8b07077e0cdbacef858f9b02ba38ab4d9cbd4
Joy Hauser <joy.hauser@hotmail.com>
2018-03-13 20:18:44 -0500

    Add functionality to add new hardware element

--- client/src/Pages/Hardware/Hardware.js
+++ client/src/Pages/Hardware/Hardware.js
@@ -1,41 +1,51 @@
 import React, { Component } from 'react';
 import HardwareElement from './HardwareElement';
-import Modal from 'react-modal';
 import fire from './../../fire';
 
 class Hardware extends Component {
 	constructor(props) {
 		super(props);
+
 		this.state = {
 			hardware: [],
-			modalIsOpen: false,
 			name: "",
 			description: "",
 			serialNum: "",
-			color: ""
+			formError: ""
 		};
 
-		this.openModal = this.openModal.bind(this);
-		this.closeModal = this.closeModal.bind(this);
+		this.addHardware = this.addHardware.bind(this);
 	}
 
-	openModal(name, description, serialNum, color) {
-		this.setState({
-			modalIsOpen: true,
-			name: name,
-			description: description,
-			serialNum: serialNum,
-			color: color
+	addHardware() {
+		var self = this;
+		var hardwareRef = fire.database().ref('/hardware/');
+
+		// Get id for new curriculum
+		var id = hardwareRef.push().path["pieces_"][1];
+
+		// Add software object to firebase DB
+		fire.database().ref('/hardware/' + id)
+		.set({
+			id: id,
+			name: self.state.name,
+			description: self.state.description,
+			serialNum: self.state.serialNum,
+			color: "#"+((1<<24)*Math.random()|0).toString(16) // Generate random color
+		}).catch(function(error) {
+			self.setState({ formError: error.code + ": " + error.message });
 		});
-	}
 
-	closeModal() {
-		this.setState({modalIsOpen: false});
+		// Clear the data in the add modal
+		this.setState({
+			name: "",
+			description: "",
+			serialNum: ""
+		});
 	}
 	
 	componentDidMount() {
 		var hardwareRef = fire.database().ref("hardware/");
-				
 		hardwareRef.orderByChild("name").on("value", (data) =>
 			this.setState({hardware: data.val()}));
 	}
@@ -43,55 +53,86 @@ class Hardware extends Component {
 	render() {
 		return (
 			<div className="Hardware">
-				<Modal
-					isOpen={this.state.modalIsOpen}
-					onRequestClose={this.closeModal}
-					contentLabel="Example Modal"
-					ariaHideApp={false}
-					className="card-modal"
-				>
-					<div className="modal-header">
-						<h2>Hardware</h2>
-						<button className="close-btn" onClick={this.closeModal}>
-							<span aria-hidden="true">X</span>
-						</button>
+				<div
+					className="modal fade"
+					id="addHardwareModal"
+					tabIndex="-1"
+					role="dialog"
+					aria-labelledby="HardwareModal"
+					aria-hidden="true">
+					<div className="modal-dialog" role="document">
+						<div className="modal-content">
+							<div className="modal-header">
+								<h5 className="modal-title" id="hardwareModalTitle">Add Hardware</h5>
+								<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<form>
+								<div className="modal-body">
+									<fieldset>
+										<label htmlFor="name">Name:</label>
+										<input
+											type="text"
+											name="name"
+											onChange={event => this.setState({name: event.target.value})}
+											value={this.state.name} />
+									</fieldset>
+									<fieldset>
+										<label htmlFor="description">Description:</label>
+										<input
+											type="text"
+											name="description"
+											onChange={event => this.setState({description: event.target.value})}
+											value={this.state.description} />
+									</fieldset>
+									<fieldset>
+										<label htmlFor="serialNum">Serial Number:</label>
+										<input
+											type="text"
+											name="serialNum"
+											onChange={event => this.setState({serialNum: event.target.value})}
+											value={this.state.serialNum} />
+									</fieldset>
+								</div>
+
+								<div className="modal-footer">
+									<button
+										type="button"
+										className="btn btn-secondary"
+										onClick={this.addHardware}
+										data-dismiss="modal">
+										Add
+									</button>
+									<button
+										type="button"
+										className="btn btn-primary"
+										data-dismiss="modal">
+										Cancel
+									</button>
+								</div>
+							</form>
+						</div>
 					</div>
-					<div className="modal-body">
-						<div className="row">
-							<div className="form-text">Name: </div>
-							<div className="form-field"><input type="text" value={this.state.name} disabled /></div>
-						</div>
-						<div className="row">
-							<div className="form-text">Description: </div>
-							<div className="form-field"><input type="text" value={this.state.description} disabled /></div>
-						</div>
-						<div className="row">
-							<div className="form-text">Serial Number: </div>
-							<div className="form-field"><input type="text" value={this.state.serialNum} disabled /></div>
-						</div>
-						<div className="row">
-							<div className="form-text">Color: </div>
-							<div className="form-field"><input type="text" value={this.state.color} disabled /></div>
-						</div>
-					</div>
-					<div className="modal-footer">
-						<div className="row">
-							<input type="submit" onClick={this.closeModal} value="OK" />
-						</div>
-					</div>
-				</Modal>
-			
+				</div>
+
 				<div className="container">
+					<button
+						type="button"
+						className="btn btn-success"
+						data-toggle="modal"
+						data-target="#addHardwareModal">
+						Add
+					</button>
 					<div className="list-container">
-						{this.state.hardware.map(hardwareElem =>
+						{Object.values(this.state.hardware).map(hardwareElem =>
 							<HardwareElement
 								key={hardwareElem.id}
 								id={hardwareElem.id}
 								name={hardwareElem.name}
 								description={hardwareElem.description}
-								serialNum={hardwareElem.serial_num}
-								color={hardwareElem.color}
-								modalHandler={this.openModal} />
+								serialNum={hardwareElem.serialNum}
+								color={hardwareElem.color} />
 						)}
 					</div>
 				</div>
