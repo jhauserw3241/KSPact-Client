7edb6956b298758e14b6e598c47bd5bcefc76d1e
Joy Hauser <joy.hauser@hotmail.com>
2018-03-13 20:06:11 -0500

    Add functionality to add new software element

--- client/src/Pages/Software/Software.js
+++ client/src/Pages/Software/Software.js
@@ -7,8 +7,42 @@ class Software extends Component {
 		super(props);
 
 		this.state = {
-			software: []
+			software: [],
+			name: "",
+			description: "",
+			link: "",
+			color: "",
+			formError: ""
 		};
+
+		this.addSoftware = this.addSoftware.bind(this);
+	}
+
+	addSoftware() {
+		var self = this;
+		var softwareRef = fire.database().ref('/software/');
+
+		// Get id for new curriculum
+		var id = softwareRef.push().path["pieces_"][1];
+
+		// Add software object to firebase DB
+		fire.database().ref('/software/' + id)
+		.set({
+			id: id,
+			name: self.state.name,
+			description: self.state.description,
+			link: self.state.link,
+			color: "#"+((1<<24)*Math.random()|0).toString(16) // Generate random color
+		}).catch(function(error) {
+			self.setState({ formError: error.code + ": " + error.message });
+		});
+
+		// Clear the data in the add modal
+		this.setState({
+			name: "",
+			description: "",
+			link: ""
+		});
 	}
 	
 	componentDidMount() {
@@ -21,9 +55,78 @@ class Software extends Component {
 	render() {
 		return (
 			<div className="Software">
+				<div
+					className="modal fade"
+					id="addSoftwareModal"
+					tabIndex="-1"
+					role="dialog"
+					aria-labelledby="addSoftwareModal"
+					aria-hidden="true">
+					<div className="modal-dialog" role="document">
+						<div className="modal-content">
+							<div className="modal-header">
+								<h5 className="modal-title" id="addSoftwareModalTitle">Modal title</h5>
+								<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<form>
+								<div className="modal-body">
+									<fieldset>
+										<label htmlFor="name">Name:</label>
+										<input
+											type="text"
+											name="name"
+											onChange={event => this.setState({name: event.target.value})}
+											value={this.state.name} />
+									</fieldset>
+									<fieldset>
+										<label htmlFor="description">Description:</label>
+										<input
+											type="text"
+											name="description"
+											onChange={event => this.setState({description: event.target.value})}
+											value={this.state.description} />
+									</fieldset>
+									<fieldset>
+										<label htmlFor="link">Link:</label>
+										<input
+											type="text"
+											name="link"
+											onChange={event => this.setState({link: event.target.value})}
+											value={this.state.link} />
+									</fieldset>
+								</div>
+								<div className="modal-footer">
+									<button
+										type="button"
+										className="btn btn-secondary"
+										onClick={this.addSoftware}
+										data-dismiss="modal">
+										Save
+									</button>
+									<button
+										type="button"
+										className="btn btn-primary"
+										data-dismiss="modal">
+										Cancel
+									</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+
 				<div className="container">
+					<button
+						type="button"
+						className="btn btn-success"
+						data-toggle="modal"
+						data-target="#addSoftwareModal">
+						Add
+					</button>
 					<div className="list-container">
-						{this.state.software.map(softwareElem =>
+						{Object.values(this.state.software).map(softwareElem =>
 							<SoftwareElement
 								key={softwareElem.id}
 								id={softwareElem.id}
