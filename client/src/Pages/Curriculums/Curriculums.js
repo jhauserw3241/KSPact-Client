b940a5e4930d7dada6990f165887aba45ed2354e
Joy Hauser <joy.hauser@hotmail.com>
2018-03-13 20:17:30 -0500

    Update add curriculum title

--- client/src/Pages/Curriculums/Curriculums.js
+++ client/src/Pages/Curriculums/Curriculums.js
@@ -65,7 +65,7 @@ class Curriculums extends Component {
 					<div className="modal-dialog" role="document">
 						<div className="modal-content">
 							<div className="modal-header">
-								<h5 className="modal-title" id="addCurriculumModalTitle">Modal title</h5>
+								<h5 className="modal-title" id="addCurriculumModalTitle">Add Curriculum</h5>
 								<button type="button" className="close" data-dismiss="modal" aria-label="Close">
 								<span aria-hidden="true">&times;</span>
 								</button>
