d9329355e92de2d2814803cef71abe3cb0bfbde2
Joy Hauser <joy.hauser@hotmail.com>
2018-03-24 00:37:22 -0500

    Add hardware request modal

--- client/src/Pages/Hardware/HardwareElement.js
+++ client/src/Pages/Hardware/HardwareElement.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import fire from './../../fire';
 import './../../CSS/Card.css';
 
+// CSS and JS for datetime picker
+import "./../../../node_modules/react-datetime/css/react-datetime.css";
+import DateTime from "./../../../node_modules/react-datetime/DateTime.js";
+
 class HardwareElement extends Component {
 	constructor(props) {
 		super(props);
@@ -11,13 +15,18 @@ class HardwareElement extends Component {
 			name: this.props.name,
 			description: this.props.description,
 			serialNum: this.props.serialNum,
-			color: this.props.color
+			color: this.props.color,
+			start: "",
+			end: "",
 		};
 
 		this.editHardware = this.editHardware.bind(this);
 		this.saveHardware = this.saveHardware.bind(this);
 		this.deleteHardware = this.deleteHardware.bind(this);
+		this.requestHardware = this.requestHardware.bind(this);
 		this.resetEdit = this.resetEdit.bind(this);
+		this.validStart = this.validStart.bind(this);
+		this.validEnd = this.validEnd.bind(this);
 	}
 
 	editHardware() {
@@ -45,8 +54,28 @@ class HardwareElement extends Component {
         fire.database().ref().update(updates);
 	}
 
+	requestHardware() {
+		
+	}
+
 	resetEdit() {
 		this.setState({allowEdits: false});
+	}
+
+	validStart( current ) {
+		if(this.state.end != "") {
+			return current.isBefore( this.state.end );
+		} else {
+			return true;
+		}
+	}
+
+	validEnd( current ) {
+		if(this.state.start != "") {
+			return current.isAfter( this.state.start );
+		} else {
+			return true;
+		}
 	}
 
 	render() {
@@ -58,7 +87,7 @@ class HardwareElement extends Component {
 			<div className="HardwareElement card">
 				<div
 					className="modal fade"
-					id={"hardwareModal-" + this.props.id}
+					id={"editHardwareModal-" + this.props.id}
 					tabIndex="-1"
 					role="dialog"
 					aria-labelledby="HardwareModal"
@@ -66,7 +95,7 @@ class HardwareElement extends Component {
 					<div className="modal-dialog" role="document">
 						<div className="modal-content">
 							<div className="modal-header">
-								<h5 className="modal-title" id="hardwareModalTitle">Edit Hardware</h5>
+								<h5 className="modal-title" id="editHardwareModalTitle">Edit Hardware</h5>
 								<button
 									type="button"
 									className="close"
@@ -136,12 +165,76 @@ class HardwareElement extends Component {
 						</div>
 					</div>
 				</div>
+
+				<div
+					className="modal fade"
+					id={"requestHardwareModal-" + this.props.id}
+					tabIndex="-1"
+					role="dialog"
+					aria-labelledby="HardwareModal"
+					aria-hidden="true">
+					<div className="modal-dialog" role="document">
+						<div className="modal-content">
+							<div className="modal-header">
+								<h5 className="modal-title" id="requestHardwareModalTitle">Request Hardware</h5>
+								<button
+									type="button"
+									className="close"
+									data-dismiss="modal"
+									aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<form>
+								<div className="modal-body">
+									<div className="form-group">
+										<label htmlFor="request-start">Start Date:</label>
+										<DateTime
+											timeFormat={false}
+											onChange={event => this.setState({ start: event._d })}
+											inputProps={{ placeholder: "Click to select a date" }}
+											isValidDate={(current) => this.validStart(current)} />
+									</div>
+									<div className="form-group">
+										<label htmlFor="request-end">End Date:</label>
+										<DateTime
+											timeFormat={false}
+											onChange={event => this.setState({ end: event._d })}
+											inputProps={{ placeholder: "Click to select a date" }}
+											isValidDate={(current) => this.validEnd(current)} />
+									</div>
+								</div>
+
+								<div className="modal-footer">
+									<button
+										type="button"
+										className="btn btn-success"
+										onClick={this.requestHardware}>
+										Send
+									</button>
+									<button
+										type="button"
+										className="btn btn-danger"
+										data-dismiss="modal">
+										Cancel
+									</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
 		
-				<div className="card-img" style={divStyle} data-toggle="modal" data-target={"#hardwareModal-" + this.props.id}></div>
-				<div className="card-text" data-toggle="modal" data-target={"#hardwareModal-" + this.props.id}>
+				<div className="card-img" style={divStyle} data-toggle="modal" data-target={"#editHardwareModal-" + this.props.id}></div>
+				<div className="card-text" data-toggle="modal" data-target={"#editHardwareModal-" + this.props.id}>
 					{this.props.name}
 				</div>
 				<div className="card-btns">
+					<button
+						className="btn btn-success hardware-request-btn"
+						data-toggle="modal"
+						data-target={"#requestHardwareModal-" + this.props.id}>
+						Request
+					</button>
 					<button className="btn btn-danger card-delete-btn" onClick={this.deleteHardware}>
 						Delete
 					</button>
