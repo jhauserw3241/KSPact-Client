f2982919bb077b46229dddb2dfa4316d59b4dcb0
Joy Hauser <joy.hauser@hotmail.com>
2018-05-05 21:19:20 -0500

    Add PanelGroup object

---
+++ client/src/Pages/Common/PanelGroup.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './../../CSS/PanelGroup.css';
+
+class PanelGroup extends Component {
+	constructor(props) {
+		super(props);
+	
+		this.state = {
+			open: false,
+		};
+	}
+
+	render() {
+		return (
+            <div className="PanelGroup">
+                <div className="Panel" style={{ display: this.state.open ? "block" : "none" }}>
+					{this.props.children}
+                </div>
+				<div className="PanelButtons">
+					<button
+						className="PanelButton"
+						onClick={() => this.setState({ open: !this.state.open })}>
+						{this.props.buttonText}
+					</button>
+				</div>
+            </div>
+		);
+	}
+}
+
+export default PanelGroup;
