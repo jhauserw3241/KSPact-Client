e70fc8be3b6f8b0db985fab15d6ba68a3739e5e6
Joy Hauser <joy.hauser@hotmail.com>
2018-05-07 11:59:54 -0500

    Add grade level selection to the add curriculum modal

--- client/src/Pages/Curriculums/AddCurriculumModal.js
+++ client/src/Pages/Curriculums/AddCurriculumModal.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import grade_levels from './../Common/GradeLevels';
 import { generateColor } from './../Common/Color';
 import fire from './../../fire';
+import CurriculumGradesTagInput from './CurriculumGradesTagInput';
+import { formatTagsForDB } from '../Common/TagsFunctions';
 
 class AddCurriculumModal extends Component {
 	constructor(props) {
@@ -11,10 +13,14 @@ class AddCurriculumModal extends Component {
 			name: "",
 			description: "",
 			link: "",
+			grade_levels: [],
 			formError: "",
 		};
 
 		this.addCurriculum = this.addCurriculum.bind(this);
+		this.handleGradeDelete = this.handleGradeDelete.bind(this)
+		this.handleGradeAddition = this.handleGradeAddition.bind(this);
+		this.handleGradeDrag = this.handleGradeDrag.bind(this);
 	}
 
 	addCurriculum() {
@@ -31,6 +37,7 @@ class AddCurriculumModal extends Component {
 			name: self.state.name,
 			description: self.state.description,
 			link: self.state.link,
+			grade_levels: formatTagsForDB(self.state.grade_levels),
 			color: generateColor(),
 		}).catch(function(error) {
 			self.setState({ formError: error.code + ": " + error.message });
@@ -40,9 +47,30 @@ class AddCurriculumModal extends Component {
 		this.setState({
 			name: "",
 			description: "",
-			link: ""
+			link: "",
+			grade_levels: [],
 		});
 	}
+
+	handleGradeDelete(i) {
+		var tags = this.state.grade_levels.filter((tag, index) => index !== i);
+		this.setState({ grade_levels: tags });
+	}
+
+    handleGradeAddition(tag) {
+		var tags = [...this.state.grade_levels, ...[tag]];
+		this.setState({ grade_levels: tags });
+    }
+
+    handleGradeDrag(tag, currPos, newPos) {
+        const tags = [...this.state.grade_levels];
+        const newTags = tags.slice();
+
+        newTags.splice(currPos, 1);
+		newTags.splice(newPos, 0, tag);
+		
+		this.setState({ grade_levels: newTags });
+    }
 
 	render() {
 		return (
@@ -89,6 +117,14 @@ class AddCurriculumModal extends Component {
 										className="form-control"
 										onChange={event => this.setState({link: event.target.value})}
 										value={this.state.link} />
+								</div>
+								<div className="form-group">
+									<label htmlFor="gradeLevels">Grade Levels:</label>
+									<CurriculumGradesTagInput
+										tags={this.state.grade_levels}
+										handleDelete={this.handleGradeDelete}
+										handleAddition={this.handleGradeAddition}
+										handleDrag={this.handleGradeDrag} />
 								</div>
 							</div>
 							<div className="modal-footer">
--- client/src/Pages/Curriculums/CurriculumGradesTagInput.js
+++ client/src/Pages/Curriculums/CurriculumGradesTagInput.js
@@ -1,7 +1,22 @@
 import React, {Component} from 'react';
-import { grade_levels } from './../Common/GradeLevels';
 import TagInput from './../Common/TagInput';
 import fire from './../../fire';    
+
+const grade_levels = [
+    { id: 'Kindergarden', text: 'Kindergarden' },
+    { id: '1st', text: '1st' },
+    { id: '2nd', text: '2nd' },
+    { id: '3rd', text: '3rd' },
+    { id: '4th', text: '4th' },
+    { id: '5th', text: '5th' },
+    { id: '6th', text: '6th' },
+    { id: '7th', text: '7th' },
+    { id: '8th', text: '8th' },
+    { id: '9th', text: '9th' },
+    { id: '10th', text: '10th' },
+    { id: '11th', text: '11th' },
+    { id: '12th', text: '12th' },
+];
 
 class CurriculumGradesTagInput extends Component {
     constructor(props) {
@@ -77,11 +92,11 @@ class CurriculumGradesTagInput extends Component {
         return (
             <div>
                 <TagInput
-                    tags={this.state.tags}
+                    tags={this.props.tags ? this.props.tags : this.state.tags}
                     suggestions={this.state.suggestions}
-                    handleDelete={this.handleDelete}
-                    handleAddition={this.handleAddition}
-                    handleDrag={this.handleDrag}
+                    handleDelete={this.props.handleDelete ? this.props.handleDelete : this.handleDelete}
+                    handleAddition={this.props.handleAddition ? this.props.handleAddition : this.handleAddition}
+                    handleDrag={this.props.handleDrag ? this.props.handleDrag : this.handleDrag}
                     readOnly={this.props.readOnly} />
             </div>
         )
